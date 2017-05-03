1fff2ff5923ee2f6c7ebe03c8904ed1f6792274b
David Meybohm <dmeybohm@gmail.com>
2017-05-03 00:23:48 -0400

    Add UI options

---
+++ src/com/daveme/intellij/organizephpimports/OrganizeImportsConfig.java
@@ -0,0 +1,63 @@
+package com.daveme.intellij.organizephpimports;
+
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.options.SearchableConfigurable;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class OrganizeImportsConfig implements SearchableConfigurable {
+
+    @NotNull
+    @Override
+    public String getId() {
+        return "organizephpimports.OrganizeImportsConfig";
+    }
+
+    @Nullable
+    @Override
+    public Runnable enableSearch(String s) {
+        return null;
+    }
+
+    @Nls
+    @Override
+    public String getDisplayName() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getHelpTopic() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+        OrganizeImportsConfigUI organizeImportsConfigUI = new OrganizeImportsConfigUI();
+        return organizeImportsConfigUI.getMyPanel();
+    }
+
+    @Override
+    public boolean isModified() {
+        return false;
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void disposeUIResources() {
+
+    }
+}
---
+++ src/com/daveme/intellij/organizephpimports/OrganizeImportsConfigUI.java
@@ -0,0 +1,14 @@
+package com.daveme.intellij.organizephpimports;
+
+import javax.swing.*;
+
+public class OrganizeImportsConfigUI {
+    private JCheckBox addAnExtraBackslashCheckBox;
+    private JPanel myPanel;
+    private JCheckBox removeUnusedUseStatementsCheckBox;
+    private JCheckBox sortUseStatementsCheckBox;
+
+    public JPanel getMyPanel() {
+        return myPanel;
+    }
+}
